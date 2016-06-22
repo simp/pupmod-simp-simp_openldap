3a9b566285d6ad375c0a921b912a0e6bb86f37bd
Nick Miller <nick.miller@onyxpoint.com>
2016-06-22 14:50:45 -0400

    (SIMP-1155) Acceptance tests for openldap
    Acceptance tests were written to cover the following situations:
    - applying the module
    - applying the module idempotently
    - add user
    - add user to admin group
    - expiring users with ppolicy
    - checking password complexity
    - enabling tls
    - rejecting non-tls connections with tls enabled
    
    These tests do not test nscd or sssd, just openldap. They also don't use
    any SIMP features.
    
    (SIMP-1179) Update client.pp to disable tls
    
    Created a template the .ldaprc file resource and put all logic for tls inside
    it, leaving an empty file if tls is disabled. openldap::client::use_tls
    can be used to disable tls on the client. Edited the ldap.conf.erb template to
    not include tls-related entries if disabled. Changed a few of the
    default values in default_ldif.pp to closer reflect password standards
    set by the pam module.
    
    SIMP-1155 #close
    SIMP-1179 #close

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,55 @@
+require 'beaker-rspec'
+require 'tmpdir'
+require 'yaml'
+require 'simp/beaker_helpers'
+include Simp::BeakerHelpers
+
+unless ENV['BEAKER_provision'] == 'no'
+  hosts.each do |host|
+    # Install Puppet
+    if host.is_pe?
+      install_pe
+    else
+      install_puppet
+    end
+  end
+end
+
+# returns an Array of puppet modules declared in .fixtures.yml
+def pupmods_in_fixtures_yaml
+  require 'yaml'
+  fixtures_yml = File.expand_path( '../.fixtures.yml', File.dirname(__FILE__))
+  data         = YAML.load_file( fixtures_yml )
+  repos        = data.fetch('fixtures').fetch('repositories').keys
+  symlinks     = data.fetch('fixtures').fetch('symlinks', {}).keys
+  (repos + symlinks)
+end
+
+
+RSpec.configure do |c|
+  # ensure that environment OS is ready on each host
+  fix_errata_on hosts
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    begin
+      # Install modules and dependencies from spec/fixtures/modules
+      copy_fixture_modules_to( hosts )
+
+      # Generate and install PKI certificates on each SUT
+      Dir.mktmpdir do |cert_dir|
+        run_fake_pki_ca_on( default, hosts, cert_dir )
+        hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
+      end
+    rescue StandardError, ScriptError => e
+      if ENV['PRY']
+        require 'pry'; binding.pry
+      else
+        raise e
+      end
+    end
+  end
+end

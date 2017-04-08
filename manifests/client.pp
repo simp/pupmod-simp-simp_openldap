0adc282a69b3d016049f8146578dff47cd5ef3d5
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2017-04-07 22:04:07 -0400

    (SIMP-2992) Work around for broken LDAP libs
    The client LDAP libraries are broken in EL6 such that, if any 128 bit
    ciphers are present in the cipher string, it will fall back to SSF=128
    for connecting to an LDAP server.
    
    This is unacceptable to the SIMP default configuration (SSF=256).
    
    Also, fixed a bug in a template with regards to variables with prefaced
    underscores.
    
    SIMP-2992 #close Update for OpenLDAP Client

--- manifests/client.pp
+++ manifests/client.pp
@@ -29,23 +29,48 @@
 # @param app_pki_crl
 #   Path to the CRL file.
 #
+# @param strip_128_bit_ciphers
+#   On EL6 systems, all 128-bit ciphers will be removed from ``tls_cipher_suite``
+#
+#   * This is due to a bug in the LDAP client libraries that does not appear to
+#     honor the order of the SSL ciphers and will attempt to connect with
+#     128-bit ciphers and not use stronger ciphers when those are present. This
+#     breaks connections to securely configured LDAP servers.
+#
 class simp_openldap::client (
-  Array[Simplib::URI]                          $uri                 = $::simp_openldap::_ldap_uri,
-  Optional[String]                             $base_dn             = $::simp_openldap::base_dn,
-  String[1]                                    $bind_dn             = $::simp_openldap::bind_dn,
-  Enum['on','off']                             $referrals           = 'on',
-  Integer                                      $sizelimit           = 0,
-  Integer                                      $timelimit           = 15,
-  Variant[Enum['simp'],Boolean]                $use_tls             = $::simp_openldap::pki,
-  Stdlib::Absolutepath                         $app_pki_ca_dir      = $::simp_openldap::app_pki_ca_dir,
-  Stdlib::Absolutepath                         $app_pki_cert        = $::simp_openldap::app_pki_cert,
-  Stdlib::Absolutepath                         $app_pki_key         = $::simp_openldap::app_pki_key,
-  Optional[Stdlib::Absolutepath]               $app_pki_crl         = $::simp_openldap::app_pki_crl,
-  Array[String[1]]                             $tls_cipher_suite    = simplib::lookup('simp_options::openssl::cipher_suite', { 'default_value' => ['DEFAULT','!MEDIUM'] }),
-  Enum['none','peer','all']                    $tls_crlcheck        = 'none',
-  Enum['never','searching','finding','always'] $deref               = 'never',
-  Enum['never','allow','try','demand','hard']  $tls_reqcert         = 'allow'
+  Array[Simplib::URI]                          $uri                   = $::simp_openldap::_ldap_uri,
+  Optional[String]                             $base_dn               = $::simp_openldap::base_dn,
+  String[1]                                    $bind_dn               = $::simp_openldap::bind_dn,
+  Enum['on','off']                             $referrals             = 'on',
+  Integer                                      $sizelimit             = 0,
+  Integer                                      $timelimit             = 15,
+  Variant[Enum['simp'],Boolean]                $use_tls               = $::simp_openldap::pki,
+  Stdlib::Absolutepath                         $app_pki_ca_dir        = $::simp_openldap::app_pki_ca_dir,
+  Stdlib::Absolutepath                         $app_pki_cert          = $::simp_openldap::app_pki_cert,
+  Stdlib::Absolutepath                         $app_pki_key           = $::simp_openldap::app_pki_key,
+  Optional[Stdlib::Absolutepath]               $app_pki_crl           = $::simp_openldap::app_pki_crl,
+  Boolean                                      $strip_128_bit_ciphers = true,
+  Array[String[1]]                             $tls_cipher_suite      = simplib::lookup('simp_options::openssl::cipher_suite', { 'default_value' => ['DEFAULT','!MEDIUM'] }),
+  Enum['none','peer','all']                    $tls_crlcheck          = 'none',
+  Enum['never','searching','finding','always'] $deref                 = 'never',
+  Enum['never','allow','try','demand','hard']  $tls_reqcert           = 'allow'
 ) inherits ::simp_openldap {
+
+  if $strip_128_bit_ciphers {
+    # This is here due to a bug in the LDAP client library on EL6 that will set
+    # the SSF to 128 when connecting over StartTLS if there are *any* 128-bit
+    # ciphers in the list.
+    if $facts['os']['name'] in ['RedHat','CentOS'] and (versioncmp($facts['os']['release']['major'],'7') < 0) {
+      $_tmp_suite = flatten($tls_cipher_suite.map |$cipher| { split($cipher,':') })
+      $_tls_cipher_suite = $_tmp_suite.filter |$cipher| { $cipher !~ Pattern[/128/] }
+    }
+    else {
+      $_tls_cipher_suite = $tls_cipher_suite
+    }
+  }
+  else {
+    $_tls_cipher_suite = $tls_cipher_suite
+  }
 
   file { '/etc/openldap/ldap.conf':
     owner   => 'root',

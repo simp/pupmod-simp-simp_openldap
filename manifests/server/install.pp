afe5e35d462782b51c39b71c313fa00c90b0e663
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2018-10-12 17:16:24 -0400

    update @author

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -5,7 +5,7 @@
 # @param ensure
 #   The state for the packages to be in
 #
-# @author Trevor Vaughan <tvaughan@onyxpoint.com>
+# @author https://github.com/simp/pupmod-simp-simp_openldap/graphs/contributors
 #
 class simp_openldap::server::install (
   Enum['latest','installed','present'] $ensure = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' }),

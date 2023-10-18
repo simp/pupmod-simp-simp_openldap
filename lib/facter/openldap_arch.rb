b073641f56388b9c495c4378b0e96705029369eb
Steven Pritchard <steve@sicura.us>
2023-10-18 09:55:49 -0500

    Remove use of FileTest.exists?
    Replace call to `FileTest.exists?` with `FileTest.exist?` for
    compatibility with Ruby 3
    
    Closes #127

--- lib/facter/openldap_arch.rb
+++ lib/facter/openldap_arch.rb
@@ -6,7 +6,7 @@ Facter.add("openldap_arch") do
     setcode do
         retval = "i386"
 
-        if FileTest.exists?("/usr/sbin/slapd") then
+        if FileTest.exist?("/usr/sbin/slapd") then
 
             if ( %x{/usr/bin/file /usr/sbin/slapd} =~ /64-bit/ ) then
                 retval = "x86_64"

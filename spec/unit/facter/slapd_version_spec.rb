ea54147cd28e5c8b4dff72f18d8bcdc000309241
Steven Pritchard <steve@sicura.us>
2024-06-18 15:47:08 -0500

    Switch from mocha to rspec mocks

--- spec/unit/facter/slapd_version_spec.rb
+++ spec/unit/facter/slapd_version_spec.rb
@@ -7,15 +7,15 @@ describe 'slapd_version' do
 
   context 'slapd command exists' do
     it 'returns the correct version of slapd' do
-      Facter::Core::Execution.stubs(:which).with('slapd').returns('/sbin/slapd')
-      Facter::Core::Execution.stubs(:execute).with('/sbin/slapd -VV 2>&1').returns("@(#) $OpenLDAP: slapd 2.4.40 (Nov  6 2016 01:21:28) $")
+      allow(Facter::Core::Execution).to receive(:which).with('slapd').and_return('/sbin/slapd')
+      allow(Facter::Core::Execution).to receive(:execute).with('/sbin/slapd -VV 2>&1').and_return("@(#) $OpenLDAP: slapd 2.4.40 (Nov  6 2016 01:21:28) $")
       expect(Facter.fact(:slapd_version).value).to eq('2.4.40')
     end
   end
 
   context 'slapd command does not exist' do
     it 'returns nil' do
-      Facter::Core::Execution.stubs(:which).with('slapd').returns(nil)
+      allow(Facter::Core::Execution).to receive(:which).with('slapd').and_return(nil)
       expect(Facter.fact(:slapd_version).value).to eq(nil)
     end
   end
